28865be30a4d4b407e286667e22b00b14b3590bc
aegis301 <57251443+aegis301@users.noreply.github.com>
2023-12-03 13:09:03 +0100

    feat: close chapter 3

--- src/app/lib/placeholder-data.js
+++ src/app/lib/placeholder-data.js
@@ -1,4 +1,5 @@
-const employess = [
+
+const employees = [
     {
         id: 1,
         first_name: 'John',
@@ -62,52 +63,45 @@ const employess = [
 
 ]
 
+const teams = [
+    {
+        id: 1,
+        name: 'Team 1',
+        employees: [1, 2, 3, 4]
+    },
+    {
+        id: 2,
+        name: 'Team 2',
+        employees: [5, 6, 7, 8]
+    },
+    {
+        id: 3,
+        name: 'Team 3',
+        employees: [9, 10]
+    }
+]
+
 const shift_templates = [
     {
         id: 1,
         name: 'Early Shift',
         start_time: '07:00',
         end_time: '16:00',
-        employees: [1, 2]
+        possible_teams: [1, 2],
     },
     {
         id: 2,
         name: 'Late Shift',
         start_time: '10:00',
         end_time: '19:00',
+        possible_teams: [1, 2],
     },
     {
         id: 3,
         name: 'Night Shift',
         start_time: '15:00',
         end_time: '00:00',
+        possible_teams: [2, 3],
     }
 ]
 
-const shifts = [
-    {
-        id: 1,
-        shift_template: 1,
-        employess: [1, 2]
-    },
-    {
-        id: 2,
-        shift_template: 2,
-        employess: [3, 4]
-    },
-    {
-        id: 3,
-        shift_template: 3,
-        employess: [5, 6]
-    },
-    {
-        id: 4,
-        shift_template: 1,
-        employess: [7, 8]
-    },
-    {
-        id: 5,
-        shift_template: 2,
-        employess: [9, 10]
-    }
-]
