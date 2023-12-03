7e8625dc9bc09f84388fdae7078dc5b6e62a6ac6
aegis301 <57251443+aegis301@users.noreply.github.com>
2023-12-03 13:13:47 +0100

    feat: adjust links

--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -1,7 +1,7 @@
 import {
-    UserGroupIcon,
     HomeIcon,
-    DocumentDuplicateIcon,
+    UserCircleIcon,
+    DocumentCheckIcon
   } from '@heroicons/react/24/outline';
   
   // Map of links to display in the side navigation.
@@ -9,11 +9,11 @@ import {
   const links = [
     { name: 'Home', href: '/dashboard', icon: HomeIcon },
     {
-      name: 'Invoices',
-      href: '/dashboard/invoices',
-      icon: DocumentDuplicateIcon,
+      name: 'Shifts',
+      href: '/dashboard/shifts',
+      icon: DocumentCheckIcon,
     },
-    { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+    { name: 'People', href: '/dashboard/teams', icon: UserCircleIcon },
   ];
   
   export default function NavLinks() {
